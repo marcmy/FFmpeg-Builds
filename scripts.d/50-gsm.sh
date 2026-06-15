e361b1dd0aa4da649d9cb7a203bf0d7943ee38ff
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-15 15:23:35 -0400

    Enable libgsm in marcshared build

--- scripts.d/50-gsm.sh
+++ scripts.d/50-gsm.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-# Placeholder: fill manually if connector keeps blocking this file.
+SCRIPT_REPO="https://github.com/HorstBaerbel/libgsm.git"
+SCRIPT_COMMIT="main"
 
 ffbuild_enabled() {
-    return -1
+    [[ $VARIANT == *marcshared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    make CC="$FFBUILD_TOOLCHAIN-gcc -ansi -pedantic" AR="$FFBUILD_TOOLCHAIN-ar" RANLIB="$FFBUILD_TOOLCHAIN-ranlib" lib/libgsm.a
+
+    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib" "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/gsm"
+    cp lib/libgsm.a "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/libgsm.a"
+    cp inc/gsm.h "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/gsm/gsm.h"
+}
+
+ffbuild_configure() {
+    echo --enable-libgsm
+}
+
+ffbuild_cflags() {
+    echo -I/ffbuild/prefix/include
+}
+
+ffbuild_libs() {
+    echo -lgsm
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libgsm
 }

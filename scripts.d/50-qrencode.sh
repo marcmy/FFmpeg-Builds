3ec2e1715535d8d7a3e7bafaf798fd47bd45249b
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-15 15:15:13 -0400

    Add libqrencode to marcshared build

---
+++ scripts.d/50-qrencode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/fukuchi/libqrencode.git"
+SCRIPT_COMMIT="v4.1.1"
+
+ffbuild_enabled() {
+    [[ $VARIANT == *marcshared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF -DWITH_TOOLS=OFF -DWITH_TESTS=OFF -DWITHOUT_PNG=ON ..
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+}
+
+ffbuild_configure() {
+    echo --enable-libqrencode
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libqrencode
+}

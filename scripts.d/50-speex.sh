60b7cf8fe71937d1d97e233e74e43e619b2764c8
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-15 15:14:40 -0400

    Add libspeex to marcshared build

---
+++ scripts.d/50-speex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/xiph/speex.git"
+SCRIPT_COMMIT="9245e0a409dd70601de92bc3859bc2c3a8cab2f0"
+
+ffbuild_enabled() {
+    [[ $VARIANT == *marcshared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    ./autogen.sh
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --host="$FFBUILD_TOOLCHAIN"
+    )
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+}
+
+ffbuild_configure() {
+    echo --enable-libspeex
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libspeex
+}

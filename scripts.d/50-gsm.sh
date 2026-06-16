5a9dfe400fb8478fe36a150925a1672665240e21
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-16 00:03:12 -0400

    Harden gsm headers and pkg-config metadata

--- scripts.d/50-gsm.sh
+++ scripts.d/50-gsm.sh
@@ -11,9 +11,23 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
     make CC="$FFBUILD_TOOLCHAIN-gcc -ansi -pedantic" AR="$FFBUILD_TOOLCHAIN-ar" RANLIB="$FFBUILD_TOOLCHAIN-ranlib" lib/libgsm.a
 
-    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib" "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/gsm"
+    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib" "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/gsm" "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig"
     cp lib/libgsm.a "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/libgsm.a"
     cp inc/gsm.h "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/gsm/gsm.h"
+    cp inc/gsm.h "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/gsm.h"
+
+    cat > "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig/gsm.pc" <<EOF
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
+
+Name: gsm
+Description: GSM 06.10 lossy speech compression
+Version: 1.0.22
+Libs: -L\${libdir} -lgsm
+Cflags: -I\${includedir}
+EOF
 }
 
 ffbuild_configure() {

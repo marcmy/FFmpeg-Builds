a19b56055f69d426ddc4b57621d6d64b4411c10d
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-16 09:20:45 -0400

    Add libshine to marcshared build

---
+++ scripts.d/50-shine.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/toots/shine.git"
+SCRIPT_COMMIT="master"
+
+ffbuild_enabled() {
+    [[ $VARIANT == *marcshared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    if [[ -x ./bootstrap ]]; then
+        ./bootstrap
+    elif [[ -x ./bootstrap.sh ]]; then
+        ./bootstrap.sh
+    elif [[ -x ./autogen.sh ]]; then
+        ./autogen.sh
+    else
+        autoreconf -fiv
+    fi
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --host="$FFBUILD_TOOLCHAIN"
+    )
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+
+    if [[ ! -f "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig/shine.pc" ]]; then
+        mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig"
+        cat > "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig/shine.pc" <<EOF
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
+
+Name: shine
+Description: shine
+Version: 3.1.1
+Libs: -L\${libdir} -lshine
+Cflags: -I\${includedir}
+EOF
+    fi
+}
+
+ffbuild_configure() {
+    echo --enable-libshine
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libshine
+}

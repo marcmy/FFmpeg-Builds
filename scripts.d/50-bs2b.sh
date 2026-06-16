a5206b71890004c9f470bc52d1f9f9df21aad891
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-16 09:19:38 -0400

    Add libbs2b to marcshared build

---
+++ scripts.d/50-bs2b.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/alexmarsev/libbs2b.git"
+SCRIPT_COMMIT="master"
+
+ffbuild_enabled() {
+    [[ $VARIANT == *marcshared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    local cc="$FFBUILD_TOOLCHAIN-gcc"
+    local ar="$FFBUILD_TOOLCHAIN-ar"
+    local ranlib="$FFBUILD_TOOLCHAIN-ranlib"
+
+    "$cc" $CFLAGS -Isrc -c src/bs2b.c -o bs2b.o
+    "$ar" rcs libbs2b.a bs2b.o
+    "$ranlib" libbs2b.a
+
+    install -Dm644 libbs2b.a "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/libbs2b.a"
+    install -Dm644 src/bs2b.h "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/bs2b.h"
+
+    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig"
+    cat > "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig/libbs2b.pc" <<EOF
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
+
+Name: libbs2b
+Description: Bauer stereophonic-to-binaural DSP library
+Version: 3.1.0
+Libs: -L\${libdir} -lbs2b
+Libs.private: -lm
+Cflags: -I\${includedir}
+EOF
+}
+
+ffbuild_configure() {
+    echo --enable-libbs2b
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libbs2b
+}

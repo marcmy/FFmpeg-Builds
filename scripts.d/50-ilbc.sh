9da60dd95dec69985545756feabfcb9ca99d6e95
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-16 09:19:50 -0400

    Add libilbc to marcshared build

---
+++ scripts.d/50-ilbc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/TimothyGu/libilbc.git"
+SCRIPT_COMMIT="master"
+
+ffbuild_enabled() {
+    [[ $VARIANT == *marcshared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    if [[ -x ./bootstrap.sh ]]; then
+        ./bootstrap.sh
+    elif [[ -x ./autogen.sh ]]; then
+        ./autogen.sh
+    else
+        autoreconf -fiv
+    fi
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --host="$FFBUILD_TOOLCHAIN"
+    )
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+}
+
+ffbuild_configure() {
+    echo --enable-libilbc
+}
+
+ffbuild_libs() {
+    echo -lilbc
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libilbc
+}

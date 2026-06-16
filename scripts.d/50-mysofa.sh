9a3ae110c7013e7adede3b8f930922558a2b5daf
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-16 12:48:31 -0400

    Use clean build directory for libmysofa

--- scripts.d/50-mysofa.sh
+++ scripts.d/50-mysofa.sh
@@ -14,16 +14,14 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerbuild() {
-    mkdir build && cd build
-
-    cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+    rm -rf ffbuild-build
+    cmake -G Ninja -S . -B ffbuild-build -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
         -DBUILD_SHARED_LIBS=OFF \
         -DBUILD_STATIC_LIBS=ON \
         -DBUILD_TESTS=OFF \
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-        ..
-    ninja -j$(nproc)
-    DESTDIR="$FFBUILD_DESTDIR" ninja install
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    ninja -C ffbuild-build -j$(nproc)
+    DESTDIR="$FFBUILD_DESTDIR" ninja -C ffbuild-build install
 
     # Be explicit for downstream static consumers. Newer CMake export headers
     # may use this define to avoid dllimport annotations with static linking.

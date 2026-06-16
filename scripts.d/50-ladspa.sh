413ba94aa6a3dc78d9b76ee789b940ef2f43926c
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-16 10:46:50 -0400

    Provide dlfcn-win32 for LADSPA

--- scripts.d/50-ladspa.sh
+++ scripts.d/50-ladspa.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+SCRIPT_REPO="https://github.com/dlfcn-win32/dlfcn-win32.git"
+SCRIPT_COMMIT="master"
+
 ffbuild_enabled() {
     [[ $VARIANT == *marcshared* ]] || return -1
-    return 0
-}
-
-ffbuild_dockerdl() {
     return 0
 }
 
 ffbuild_dockerbuild() {
     apt-get -y update
     apt-get -y install --no-install-recommends ladspa-sdk
+
+    mkdir build && cd build
+    cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        ..
+    ninja -j$(nproc)
+    DESTDIR="$FFBUILD_DESTDIR" ninja install
 
     install -Dm644 /usr/include/ladspa.h "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/ladspa.h"
 
@@ -21,6 +27,10 @@ ffbuild_dockerbuild() {
 
 ffbuild_configure() {
     echo --enable-ladspa
+}
+
+ffbuild_libs() {
+    echo -ldl
 }
 
 ffbuild_unconfigure() {

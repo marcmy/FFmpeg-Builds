deef4bdf8ccdd4404587ecb9a6d1ca44e81b33e4
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-15 22:09:12 -0400

    Fix qrencode build with newer CMake

--- scripts.d/50-qrencode.sh
+++ scripts.d/50-qrencode.sh
@@ -12,6 +12,7 @@ ffbuild_dockerbuild() {
     mkdir build && cd build
 
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
         -DBUILD_SHARED_LIBS=OFF -DWITH_TOOLS=OFF -DWITH_TESTS=OFF -DWITHOUT_PNG=ON ..
     make -j$(nproc)
     make install DESTDIR="$FFBUILD_DESTDIR"

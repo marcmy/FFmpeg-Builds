a03038e61bd5f72f75c9add3025aca8f0622b0bb
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-17 17:34:58 -0400

    Rename marcshared variant guard for libffi

--- scripts.d/50-libffi.sh
+++ scripts.d/50-libffi.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/libffi/libffi.git"
 SCRIPT_COMMIT="master"
 
 ffbuild_enabled() {
-    [[ $VARIANT == *marcshared* ]] || return -1
+    [[ $VARIANT == *marc-shared* ]] || return -1
     return 0
 }
 

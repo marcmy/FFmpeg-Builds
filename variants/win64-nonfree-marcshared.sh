82d9194eb97b25ca0304c543bd825948b50e6669
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-17 17:37:21 -0400

    Remove old nonfree-marcshared variant

--- variants/win64-nonfree-marcshared.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Marc's custom nonfree shared Windows build variant.
-# Starts as BtbN win64-nonfree-shared; extra deps can be layered in later.
-source "$(dirname "$BASH_SOURCE")"/win64-nonfree-shared.sh

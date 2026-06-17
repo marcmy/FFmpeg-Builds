9ffbb76bf62b07c86f8351461cb4c2d920b79550
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-17 17:31:01 -0400

    Rename marcshared variant guard for LADSPA

--- scripts.d/50-ladspa.sh
+++ scripts.d/50-ladspa.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/dlfcn-win32/dlfcn-win32.git"
 SCRIPT_COMMIT="master"
 
 ffbuild_enabled() {
-    [[ $VARIANT == *marcshared* ]] || return -1
+    [[ $VARIANT == *marc-shared* ]] || return -1
     return 0
 }
 

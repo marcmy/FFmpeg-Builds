5c0a65d2966b0ac7b5d124bfbf2c89d02d6e8b23
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-18 17:07:20 -0400

    Enable GSM for marc-shared variant

--- scripts.d/50-gsm.sh
+++ scripts.d/50-gsm.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/HorstBaerbel/libgsm.git"
 SCRIPT_COMMIT="main"
 
 ffbuild_enabled() {
-    [[ $VARIANT == *marcshared* ]] || return -1
+    [[ $VARIANT == *marc-shared* ]] || return -1
     return 0
 }
 

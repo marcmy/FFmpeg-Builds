7382dd4413a1cf6b04c0716f5083120c682ff08c
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-18 17:07:06 -0400

    Enable Speex for marc-shared variant

--- scripts.d/50-speex.sh
+++ scripts.d/50-speex.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/xiph/speex.git"
 SCRIPT_COMMIT="9245e0a409dd70601de92bc3859bc2c3a8cab2f0"
 
 ffbuild_enabled() {
-    [[ $VARIANT == *marcshared* ]] || return -1
+    [[ $VARIANT == *marc-shared* ]] || return -1
     return 0
 }
 

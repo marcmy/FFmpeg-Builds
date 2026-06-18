9fd71bf70cf49c7898db40bd44f1fc0570f6a18c
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-17 20:17:53 -0400

    Add XEVE encoder to marc-shared build

---
+++ scripts.d/50-xeve.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/mpeg5/xeve.git"
+SCRIPT_COMMIT="master"
+
+ffbuild_enabled() {
+    [[ $VARIANT == *marc-shared* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    # Shallow/minimal clones may not have tags. Upstream CMake requires either
+    # a git tag or version.txt matching vMAJOR.MINOR.PATCH.
+    [[ -f version.txt ]] || echo v0.5.0 > version.txt
+
+    rm -rf ffbuild-build
+    cmake -G Ninja -S . -B ffbuild-build -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DSET_PROF=MAIN \
+        -DXEVE_APP_STATIC_BUILD=ON \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    ninja -C ffbuild-build -j$(nproc)
+    DESTDIR="$FFBUILD_DESTDIR" ninja -C ffbuild-build install || true
+
+    local lib
+    lib="$(find ffbuild-build -name 'libxeve.a' -type f -print -quit)"
+    [[ -n "$lib" ]] || return -1
+    install -Dm644 "$lib" "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/libxeve.a"
+
+    find inc -name '*.h' -type f -print0 | while IFS= read -r -d '' header; do
+        install -Dm644 "$header" "$FFBUILD_DESTDIR$FFBUILD_PREFIX/include/$(basename "$header")"
+    done
+
+    mkdir -p "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig"
+    cat > "$FFBUILD_DESTDIR$FFBUILD_PREFIX/lib/pkgconfig/xeve.pc" <<EOF
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
+
+Name: xeve
+Description: eXtra-fast Essential Video Encoder, MPEG-5 EVC
+Version: 0.5.0
+Libs: -L\${libdir} -lxeve
+Libs.private: -lm
+Cflags: -DXEVE_STATIC_DEFINE -I\${includedir}
+EOF
+}
+
+ffbuild_configure() {
+    echo --enable-libxeve
+}
+
+ffbuild_cflags() {
+    echo -DXEVE_STATIC_DEFINE
+}
+
+ffbuild_libs() {
+    echo -lm
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libxeve
+}
